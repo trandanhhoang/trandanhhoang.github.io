beb87655e530a5ce713912ce45e47e819f4c93da
hoangtranfinx <hoang.tran@galaxyfinx.com>
2023-12-17 12:17:24 +0700

    second commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,17 +11,18 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
+  deploymentBranch:"main",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://trandanhhoang.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'trandanhhoang', // Usually your GitHub org/user name.
+  projectName: 'trandanhhoang.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
